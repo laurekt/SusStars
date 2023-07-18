20211f1dcdf99d939b2041988356be6f31e8f378
laurekt <laurenvien2006@gmail.com>
2023-07-18 10:21:08 -0700

    Initial Commit

---
+++ SusStars/SusStarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SusStarsApp.swift
+//  SusStars
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SusStarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
